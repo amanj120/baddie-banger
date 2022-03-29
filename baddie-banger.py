8d93ba3a53b1e034081b58d1930f312a4e0fb4d7
Aman Jain <aman.jain@messagegears.com>
2022-03-29 16:05:28 -0400

    file-based persistence is working, eventually i want to migrate to some nosql backend

--- baddie-banger.py
+++ baddie-banger.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 from datetime import datetime
+import json
+import jsonschema
 
 app = Flask(__name__)
+
+with open("persistence/schemas/rating-schema.json", "r") as _ratingSchemaFile:
+    ratingSchema = json.load(_ratingSchemaFile)
+    _ratingSchemaFile.close()
+with open("persistence/schemas/artist-schema.json", "r") as _artistSchemaFile:
+    artistSchema = json.load(_artistSchemaFile)
+    _artistSchemaFile.close()
+
+with open("persistence/ratings.json", "r") as _ratingsFile:
+    ratings = json.load(_ratingsFile)
+    _ratingsFile.close()
+with open("persistence/artists.json", "r") as _artistsFile:
+    artists = json.load(_artistsFile)
+    _artistsFile.close()
 
 
 @app.route("/heartbeat", methods=['GET'])
@@ -13,7 +29,18 @@ def heartbeat():
 
 @app.route("/rate-artist", methods=['POST', 'PATCH'])
 def rate_artist():
-    return "rate artist"
+    # todo: populate timestamp and rating id yourself
+    rating = request.json
+    try:
+        jsonschema.validate(instance=rating, schema=ratingSchema)
+        ratings.append(rating)
+        print(ratings)
+        with open("persistence/ratings.json", "w") as ratingsFile:
+            json.dump(ratings, ratingsFile)
+            ratingsFile.close()
+        return "rating successfully recorded"
+    except jsonschema.exceptions.ValidationError as err:
+        return "rating has following error: {}".format(err)
 
 
 @app.route("/my-ratings", methods=['GET'])
@@ -41,10 +68,12 @@ def admin_add_artist():
     pass
 
 
-@app.route("admin-update-artist", methods=['POST'])
+@app.route("/admin-update-artist", methods=['POST'])
 def admin_update_artist():
     pass
 
 
 if __name__ == "__main__":
+    # todo: make this as part of the init file or something
+
     app.run()
