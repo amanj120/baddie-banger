a0f992438af3660a105835746c991674ce988bbb
Aman Jain <aman.jain@messagegears.com>
2022-03-30 12:17:00 -0400

    added metadata for some stuff and continued to build up API

--- baddie-banger.py
+++ baddie-banger.py
@@ -18,6 +18,9 @@ with open("persistence/ratings.json", "r") as _ratingsFile:
 with open("persistence/artists.json", "r") as _artistsFile:
     artists = json.load(_artistsFile)
     _artistsFile.close()
+with open("persistence/metadata.json", "r") as _metadataFile:
+    metadata = json.load(_metadataFile)
+    _metadataFile.close()
 
 
 @app.route("/heartbeat", methods=['GET'])
@@ -32,15 +35,25 @@ def rate_artist():
     # todo: populate timestamp and rating id yourself
     rating = request.json
     try:
+        rating_id = metadata["current_rating"]
+        rating["id"] = rating_id
+        rating["timestamp"] = datetime.now().isoformat()
         jsonschema.validate(instance=rating, schema=ratingSchema)
+
+        metadata["current_rating"] = rating_id + 1
+        with open("persistence/metadata.json", "w") as metadataFile:
+            json.dump(metadata, metadataFile)
+            metadataFile.close()
+
         ratings.append(rating)
-        print(ratings)
         with open("persistence/ratings.json", "w") as ratingsFile:
             json.dump(ratings, ratingsFile)
             ratingsFile.close()
+
         return "rating successfully recorded"
     except jsonschema.exceptions.ValidationError as err:
-        return "rating has following error: {}".format(err)
+        print(err)
+        return "rating has following error: {}".format(str(err))
 
 
 @app.route("/my-ratings", methods=['GET'])
@@ -73,7 +86,23 @@ def admin_update_artist():
     pass
 
 
-if __name__ == "__main__":
-    # todo: make this as part of the init file or something
+@app.route("/admin-dev-reset", methods=['POST'])
+def admin_dev_reset():
+    metadata["current_rating"] = 0
+    with open("persistence/metadata.json", "w") as metadataFile:
+        json.dump(metadata, metadataFile)
+        metadataFile.close()
 
+    with open("persistence/ratings.json", "w") as ratingsFile:
+        json.dump([], ratingsFile)
+        ratingsFile.close()
+
+    with open("persistence/artists.json", "w") as artistsFile:
+        json.dump([], artistsFile)
+        artistsFile.close()
+
+    return "all databases reset"
+
+
+if __name__ == "__main__":
     app.run()
